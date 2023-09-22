c9de14da899c621a0d13aef700fc054cba78de23
Thomas <thomasdevsp@gmail.com>
2023-09-22 15:15:55 -0300

    Ultima aula

--- 2_MENU_RESPONSIVO/src/App.jsx
+++ 2_MENU_RESPONSIVO/src/App.jsx
@@ -2,7 +2,10 @@ import Header from "./components/Header"
 
 function App() {
   return (
+    <div>
     <Header />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor exercitationem id, temporibus, odit unde voluptatum perspiciatis architecto nihil, laudantium alias repellendus itaque cumque placeat quasi et possimus? Fugiat, eaque id. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates officia debitis consequuntur vitae commodi, soluta minima numquam, natus est illo id quas, incidunt quisquam. Reprehenderit iure non aperiam facilis perspiciatis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate distinctio nam vel optio tenetur? Distinctio quidem nisi totam quis! Et temporibus optio accusamus quidem ipsum ea qui! Corporis, suscipit illo.</p>
+    </div>
   )
 }
 
--- 2_MENU_RESPONSIVO/src/components/Header.jsx
+++ 2_MENU_RESPONSIVO/src/components/Header.jsx
@@ -1,18 +1,36 @@
-import { List  } from '@phosphor-icons/react'
+import { List, X } from '@phosphor-icons/react'
+import { useState } from 'react'
 
 import styles from "./Header.module.css"
 
 function Header() {
+  const [abrirMenu, setAbrirMenu] = useState(false)
+
+  function handleAbrirFecharMenu(){
+    if (abrirMenu === true) {
+      setAbrirMenu(false)
+      return
+    }
+    
+    setAbrirMenu(true)
+  }
+
   return (
     <header>
-      <div className='mobile'>
+      <div className={styles.mobile}>
         <div className={styles.menu_logo}>
-          <button>
-            <List size={32} />
+          <button onClick={handleAbrirFecharMenu}>
+            {
+              abrirMenu === true ? (
+                <X size={32} />
+              ) : (
+                <List size={32} />
+              )
+            }
           </button>
           <h1>Logo</h1>
         </div>
-        <div className={styles.opcoes}>
+        <div className={`${styles.options} ${abrirMenu === true && styles.open}`}>
           <nav>
             <a href="">Home</a>
             <a href="">Ferramentos</a>
@@ -21,6 +39,24 @@ function Header() {
           </nav>
 
         </div>
+      </div>
+
+      <div className={styles.desktop}>
+        
+        <div>
+          <h1>Logo</h1>
+        </div>
+        
+        <div>
+          <nav className={styles.desktop_options}>
+            <a href="">Home</a>
+            <a href="">Ferramentos</a>
+            <a href="">Objetivos</a>
+            <a href="">Bibliografia</a>
+          </nav>
+
+        </div>
+        
       </div>
     </header>
   )
